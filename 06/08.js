003306d82d1eee7be9f5a18d2029c3bf2c7c0554
dosang12 <knight2671@gmail.com>
2023-01-11 13:41:08 +0900

    20230111 수업

---
+++ 06/08.js
@@ -0,0 +1,9 @@
+//section 변수에 담기
+const section=document.querySelector('section')
+//변수에 담아야 할 것들
+const text=document.querySelector(".txt")
+const progressBar=document.querySelector(".bar")
+//창 높이 
+const windowHeight=window.innerHeight();
+//스크롤 양을 담을 변수
+let scrollTop;
