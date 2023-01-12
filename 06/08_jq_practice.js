91f003d94962ba94a4c466b86d198b01f7453654
dosang12 <knight2671@gmail.com>
2023-01-12 10:36:28 +0900

    20230112 수업 git연결

--- 06/08_jq_practice.js
+++ 06/08_jq_practice.js
@@ -10,15 +10,26 @@ const text=$(".txt")
 const windowHeight=$(window).height();
 let scrollTop;
 
-$(window).scroll(function(){
-    scrollTop=$(window).scrollTop();
-
-    let scrollHeight = section.height();
+//퍼센트 계산
+function getPercent(sct) {
+	let scrollHeight = section.height();
 	let scrollRealHeight = scrollHeight - windowHeight;
-	let scrollPercent = Math.floor((scrollTop / scrollRealHeight) * 100);
+	let scrollPercent =(sct / scrollRealHeight) * 100;
+	let textPercent = Math.floor((sct / scrollRealHeight) * 100);
 	if (scrollPercent >= 100) {
 		scrollPercent = 100;
 	}
-	text.text(scrollPercent + "%");
-	progressBar.css('width' , scrollPercent + "%");
+	render(scrollPercent,textPercent);
+}
+
+//화면에 출력
+function render(scrollPercent,t) {
+	text.text(t + "%");
+	progressBar.css("width", scrollPercent + "%");
+}
+
+//스크롤 양 얻기
+$(window).scroll(function () {
+	scrollTop = $(this).scrollTop();
+	getPercent(scrollTop);
 });
