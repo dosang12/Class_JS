63b0fe3d6e878039db9ed6326d12e99aa2cd7281
dosang12 <knight2671@gmail.com>
2023-01-17 18:14:27 +0900

    20230117 수업

---
+++ 08/ui.js
@@ -0,0 +1,38 @@
+$(function(){
+    $("#departure").datepicker({
+        dateFormat:"yy-dd-mm", //input 창에 value표시 방법.
+        showOtherMonths:true, //30 or 31 일 이후 빈공간에 다음달 날짜를 표시 or 안표시. 
+        // showMonthAfterYear:true,
+        // showWeek:true,
+        changeYear:true,
+        changeMonth:true,
+        showOn:"both",
+        buttonImage:"url(C:\Users\Administrator\Downloads\Calendar)",
+        buttonImageOnly:true,
+        yearSuffix:"년",
+        monthNames:["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월",],
+        monthNamesShort:["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월",],
+        dayNamesMin:["일","월","화","수","목","금","토"],
+        showAnim:"bounce"
+    })
+    $("#airport").autocomplete({
+        source:airports
+    })
+    $("#meal").selectmenu({
+        width:400,
+        height:500,
+    })
+    $("#bags").css('display','block').buttonset()
+    $("#seatTypes").buttonset()
+    $("#next").button({
+        icons:{secondary:"ui-icon-circle-arrow-e"}
+    })
+    $("#telme").dialog({
+        autoOpen:false,
+        width:500,
+    })
+    $("#telmeLink").click(function(e){
+        e.preventDefault();
+        $("#telme").dialog("open",$("p[title]").tooltip())
+    })
+})
