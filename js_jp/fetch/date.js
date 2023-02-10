0747878369c6dcedfb81ede73217f6e5d1f7ef7c
dosang12 <knight2671@gmail.com>
2023-02-10 17:26:00 +0900

    20230210 수업

---
+++ js_jp/fetch/date.js
@@ -0,0 +1,8 @@
+let date = new Date();
+
+let year = String(date.getFullYear());
+let month = String(date.getMonth() + 1).padStart(2, "0");
+let day = String(date.getDate()).padStart(2, "0");
+
+let now = year + month + day;
+console.log(now);
